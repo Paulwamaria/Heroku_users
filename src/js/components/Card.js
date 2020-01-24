792cc0e5cdd588dbea9e419cc3002d7533a6c2cd
Paulwamaria <paulwamaria@gmail.com>
2020-01-24 13:28:00 +0300

    Create a card component for displaying the user info

---
+++ src/js/components/Card.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class Card extends Component {
+    render() {
+        return (
+            <div>
+               <img src="" /> 
+               <p>User info go here</p>
+            </div>
+        )
+    }
+}
+
+export default Card
+
+
