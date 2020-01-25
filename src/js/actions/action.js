d9b91f46ccf772cdc8e6bd1919c466256b944bbd
Paulwamaria <paulwamaria@gmail.com>
2020-01-25 13:12:38 +0300

    Create the three main actions

---
+++ src/js/actions/action.js
@@ -0,0 +1,28 @@
+
+import {
+    FETCH_USERS_FAILURE,
+    FETCH_USERS_REQUEST,
+    FETCH_USERS_SUCCESS
+} from "../constants/UserTypes";
+
+
+const fetchUsersRequest = () => {
+    return {  
+        type: FETCH_USERS_REQUEST
+    }
+}
+
+
+const fetchUsersSuccess = users => {
+    return {
+        type: FETCH_USERS_SUCCESS,
+        payload: users
+    }
+}
+
+const fetchUsersFailure = error => {
+    return {
+        type: FETCH_USERS_FAILURE,
+        payload: error
+    }
+}
