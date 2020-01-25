676beac6cc194c54dc1571c02403dbc189a55677
Paulwamaria <paulwamaria@gmail.com>
2020-01-25 13:11:24 +0300

    Modify the user reducer to include three states

--- src/js/reducers/index.js
+++ src/js/reducers/index.js
@@ -1,26 +1,39 @@
+
+import {
+    FETCH_USERS_REQUEST,
+    FETCH_USERS_SUCCESS,
+    FETCH_USERS_FAILURE
+}from "../actions/action";
+
 let initialState = {
-    id:5,
-    name:"Isaac Nyakoi",
-    occupation: "Software engineer",
-    email: "izonyakoi@gmail.com",
-    bio: "test", 
-    created_at: "2019-07-05T10:22:22.609Z",
-    updated_at:"2020-01-23T05:48:41.524Z"
+ loading: false,
+ users: [],
+ error: ''
 }
 
-const mainReducer = (state=initialState, action)=>{
-    if (action.type==="CHANGE_USER"){
-        return{
-            ...state,
-            id:action.id,
-            name: action.name,
-            occupation: action.occupation,
-            email: action.email,
-            bio: action.bio,
-            created_at: action.created_at,
-            updated_at:action.updated_at
-        }
+
+const userReducer = (state=initialState, action) => {
+    switch(action.type){
+        case FETCH_USERS_REQUEST:
+            return{
+                ...state,
+                loading:true
+            }
+
+        case FETCH_USERS_SUCCESS:
+            return{
+                loading: false,
+                users: action.payload,
+                error:""
+            }
+
+        case FETCH_USERS_FAILURE:
+            return{
+                loading: false,
+                users: [],
+                payload: action.payload
+            }
     }
 }
 
-export default mainReducer
+export default userReducer
---
+++ src/js/store/index.js
@@ -0,0 +1,11 @@
+import { createStore, applyMiddleware } from "redux";
+import mainReducer from '../reducers/mainReducer';
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+
+const store =createStore(
+    mainReducer,
+    composeWithDevTools(applyMiddleware(thunk))
+)
+
+export default store
