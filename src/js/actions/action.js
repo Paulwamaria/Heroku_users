66bfac1bc7074a26d93be7e48eeeab3bfb6709e0
Paulwamaria <paulwamaria@gmail.com>
2020-01-26 15:42:07 +0300

    create a function to get data from the API

--- src/js/actions/action.js
+++ src/js/actions/action.js
@@ -1,8 +1,9 @@
+import axios from "axios";
 
 import {
-    FETCH_USERS_FAILURE,
     FETCH_USERS_REQUEST,
-    FETCH_USERS_SUCCESS
+    FETCH_USERS_SUCCESS,
+    FETCH_USERS_FAILURE,
 } from "../constants/UserTypes";
 
 
@@ -13,16 +14,33 @@ const fetchUsersRequest = () => {
 }
 
 
-const fetchUsersSuccess = users => {
+ const fetchUsersSuccess = users => {
     return {
         type: FETCH_USERS_SUCCESS,
         payload: users
     }
 }
 
-const fetchUsersFailure = error => {
+  const fetchUsersFailure = error => {
     return {
         type: FETCH_USERS_FAILURE,
         payload: error
+    }
+}
+
+export const fetchUsers = ()=> {
+
+    return (dispatch)=>{
+        dispatch(fetchUsersRequest)
+      axios.get("https://ti-react-test.herokuapp.com/users")
+      .then(response => {
+          console.log(response.data)
+        const users = response.data
+        dispatch(fetchUsersSuccess(users))
+      })  
+      .catch(error => {
+          const errorMsg = error.message
+          dispatch(fetchUsersFailure(errorMsg))
+      })
     }
 }
